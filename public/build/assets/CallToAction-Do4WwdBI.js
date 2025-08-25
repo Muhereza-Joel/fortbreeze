063d00e553583afc3487d259af472c3017a83d08
Muhereza-Joel <muherezajoel40@gmail.com>
2025-08-25 21:11:06 +0300

    update build

---
+++ public/build/assets/CallToAction-Do4WwdBI.js
@@ -0,0 +1,6 @@
+import{c as g}from"./PublicLayout-Cy-kv0-c.js";import{j as e,L as b}from"./app-Dd6J_HHl.js";/**
+ * @license lucide-react v0.539.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const f=[["path",{d:"M3 2v7c0 1.1.9 2 2 2h4a2 2 0 0 0 2-2V2",key:"cjf0a3"}],["path",{d:"M7 2v20",key:"1473qp"}],["path",{d:"M21 15V2a5 5 0 0 0-5 5v6c0 1.1.9 2 2 2h3Zm0 0v7",key:"j28e5"}]],v=g("utensils",f);function k({title:t,text:i,buttonText:s,buttonLink:n,bgColor:o="bg-indigo-600",darkBgColor:a="dark:bg-indigo-800",textColor:d="text-white",subTextColor:c="text-indigo-100",buttonBgColor:r="bg-white",buttonTextColor:l="text-indigo-600",buttonHoverBg:h="hover:bg-indigo-50",paddingY:x="py-12",paddingX:m="px-6",rounded:p="rounded-none",shadow:$="shadow-lg"}){return e.jsxs("div",{className:`${o} ${a} ${x} ${m} text-center ${p} ${$}`,children:[e.jsx("h2",{className:`text-3xl font-bold ${d} mb-4`,children:t}),e.jsx("p",{className:`${c} mb-6`,children:i}),e.jsx(b,{href:n,className:`px-6 py-3 ${r} ${l} font-semibold shadow ${h} transition`,children:s})]})}export{k as C,v as U};
