2ffbbd48b68f3e714362a389745dc02b69f6df47
Muhereza-Joel <muherezajoel40@gmail.com>
2025-08-30 01:24:46 +0300

    update build

---
+++ public/build/assets/SectionWrapper-CECimwkj.js
@@ -0,0 +1,1 @@
+import{j as s}from"./app-DxgkjhSl.js";function o({children:x,className:p=""}){return s.jsx("section",{className:`max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 ${p}`,children:x})}export{o as S};
